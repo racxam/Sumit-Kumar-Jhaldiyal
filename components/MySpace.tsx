cf9a47757e67d6caa618bb1b75ae6bedf9711549
Sumit Jhaldiyal <sumitracxam@gmail.com>
2024-07-14 14:05:01 +0530

    added the function

--- components/MySpace.tsx
+++ components/MySpace.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function MySpace() {
   return (
-    <main>
+    <main className='w-screen'>
       <Spline
         scene="https://prod.spline.design/siVo6XF-Ptwbapsd/scene.splinecode" 
       />
